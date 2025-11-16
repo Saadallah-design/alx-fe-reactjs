8216fac8b5060cb2730ff0fe3a9df480d3d66a31
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-17 00:17:19 +0700

    adding functionality of adding and removing recipe

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,5 +4,16 @@ import { create } from 'zustand';
  export const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  setRecipes: (recipes) => set({ recipes }),
+  updateRecipe: (id, updatedRecipeData) => set((state) => ({
+    recipes: state.recipes.map((recipe) => 
+      recipe.id === id ? { ...recipe, ...updatedRecipeData } : recipe),
+  })),
+  deleteRecipe: (id) => set((state) => ({
+    recipes: state.recipes.filter((recipe) => recipe.id !== id),
+  })),
 }));
+
+// when using zustand and trying to export 
+// it does not support the export default name at the end
+// but use export at the start: export const useRecipeStore
