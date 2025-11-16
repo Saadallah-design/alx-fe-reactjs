980071428ff60b4bc8f246cf74f210f3e5e77dd9
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-16 23:14:44 +0700

    renaming the recipeStore.js instead of jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
-import RecipeStore from './components/recipeStore'
 import './App.css'
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <>
     <RecipeList />
     <AddRecipeForm />
-    <RecipeStore />
     </>
   )
 }
