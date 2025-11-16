7fa90ffab098ec1beb46e3f60bebc2156370552b
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-17 00:43:41 +0700

    managing the routing in the App.jsx instead of main.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-import AddRecipeForm from './components/AddRecipeForm'
-import RecipeList from './components/RecipeList'
-import './App.css'
+import React from 'react';
+import { 
+  BrowserRouter, 
+  Routes, 
+  Route 
+} from 'react-router-dom';
+
+// Import all components used in your routes/layout
+import AddRecipeForm from './components/AddRecipeForm.jsx';
+import RecipeList from './components/RecipeList.jsx';
+import RecipeDetails from './components/RecipeDetails.jsx';
+
+// Simple layout component for the main page
+const Home = () => (
+  <>
+    <h1>Recipe Sharing App</h1>
+    <AddRecipeForm />
+    <hr />
+    <RecipeList />
+  </>
+);
 
 function App() {
   return (
-    <>
-    <RecipeList />
-    <AddRecipeForm />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />        
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,37 +1,13 @@
-import { StrictMode } from 'react' 
-import { createRoot } from 'react-dom/client'
+
 import './index.css'
 import App from './App.jsx'
-import AddRecipeForm from './components/AddRecipeForm.jsx'
-import RecipeList from './components/RecipeList.jsx'
-import RecipeDetails from './components/RecipeDetails.jsx'
+
 import ReactDOM from 'react-dom/client';
 import React from 'react';
-import { 
-  BrowserRouter, 
-  Routes, 
-  Route 
-} from 'react-router-dom';
-
-// A simple layout component to hold the forms
-const Home = () => (
-  <>
-    <h1>Recipe Sharing App</h1>
-    <AddRecipeForm />
-    <hr />
-    <RecipeList />
-  </>
-);
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipes/:recipeId" element={<RecipeDetails />} />        
-        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
+    {/* Rendering the App component, which now contains all routing */}
+    <App /> 
   </React.StrictMode>,
 );
