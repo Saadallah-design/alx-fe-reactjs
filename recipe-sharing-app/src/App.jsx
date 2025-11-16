0b6ce918b755ca4ed19cd18f54ea8cfa03b1bf11
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-16 23:12:40 +0700

    creating the RecipeStore file for listing the recipes available

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
+import RecipeStore from './components/recipeStore'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
     <RecipeList />
     <AddRecipeForm />
+    <RecipeStore />
     </>
   )
 }
---
+++ recipe-sharing-app/src/components/recipeStore.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import RecipeList from './RecipeList'
+
+const RecipeStore = () => {
+  return (
+    <div>
+        <h1>The current Recipe Store consists of: </h1>
+      <RecipeList />
+    </div>
+  )
+}
+
+export default RecipeStore;
